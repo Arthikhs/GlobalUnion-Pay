b29cbab64225823acb09d14a5df7d5b7c430f229
Arthikhs <hsarthik5@gmail.com>
2026-07-29 23:11:41 +0530

    docs: update README + remove node_modules from tracking

--- GlobalUnionPay-UPI/backend/src/main/java/com/globalunion/pay/repository/LoanRepository.java
+++
@@ -1,6 +0,0 @@
-package com.globalunion.pay.repository;
-
-import com.globalunion.pay.model.Loan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoanRepository extends JpaRepository<Loan, Long> {}
